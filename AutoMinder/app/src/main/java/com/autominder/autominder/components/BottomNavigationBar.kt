12c1cc2f6cd73a832532c25de29a59807ae195b6
melvin <melvin@debian-BULLSEYE-live-builder-AMD64>
2023-05-04 10:57:18 -0600

    Fix: Gradle issue

--- AutoMinder/app/src/main/java/com/autominder/autominder/components/BottomNavigationBar.kt
+++ AutoMinder/app/src/main/java/com/autominder/autominder/components/BottomNavigationBar.kt
@@ -1,30 +1,20 @@
 package com.autominder.autominder.components
 
-import androidx.compose.foundation.background
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.autominder.autominder.navigation.Destinations
-import com.autominder.autominder.ui.theme.AutoMinderTheme
-import com.autominder.autominder.ui.theme.md_theme_light_background
-import com.autominder.autominder.ui.theme.md_theme_light_secondaryContainer
-import com.autominder.autominder.ui.theme.md_theme_light_tertiaryContainer
 
 @Composable
 fun BottomNavigationBar(navHostController: NavHostController, items: List<Destinations>) {
     val currentRoute = currentRoute(navHostController)
 
-    BottomNavigation(
-        backgroundColor = androidx.compose.material3.MaterialTheme.colorScheme.secondaryContainer, 
-
-        )
+    BottomNavigation(backgroundColor = androidx.compose.material3.MaterialTheme.colorScheme.secondaryContainer)
     {
         items.forEach { screen ->
             BottomNavigationItem(
@@ -41,8 +31,6 @@ fun BottomNavigationBar(navHostController: NavHostController, items: List<Destin
             )
         }
     }
-
-
 }
 
 @Composable
