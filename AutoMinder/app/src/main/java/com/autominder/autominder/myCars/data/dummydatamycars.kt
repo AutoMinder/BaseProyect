8c0cc4455deeaf313556dbff080e5a218a46320f
melvin <melvin@debian-BULLSEYE-live-builder-AMD64>
2023-05-09 17:16:58 -0600

    fixing dummy data to have sense

--- AutoMinder/app/src/main/java/com/autominder/autominder/myCars/data/dummydatamycars.kt
+++ AutoMinder/app/src/main/java/com/autominder/autominder/myCars/data/dummydatamycars.kt
@@ -1,6 +1,6 @@
 package com.autominder.autominder.myCars.data
 
 val myCarsdummy = listOf<CarDataModel>(
-    CarDataModel("sdlfkj", "toyota", "yaris", "2001", "", "", "", false),
-    CarDataModel("", "", "", "", "", "", "", false),
-)
+    CarDataModel("sdlfkj", "toyota", "yaris", "2001", "100", "01/01/2002", "Melvin", false),
+
+    )
