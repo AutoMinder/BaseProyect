dae2d95bf3916068c72c4b13a1d38b8bef24808a
MelvinDiaz <00008821@uca.edu.sv>
2023-05-13 22:32:33 -0600

    Fix: Better design for my car card

--- AutoMinder/app/src/main/java/com/autominder/autominder/myCars/ui/MyCarsScreen.kt
+++ AutoMinder/app/src/main/java/com/autominder/autominder/myCars/ui/MyCarsScreen.kt
@@ -1,11 +1,16 @@
 package com.autominder.autominder.myCars.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -22,11 +27,18 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.autominder.autominder.R
 import com.autominder.autominder.myCars.data.CarDataModel
+
 
 @Composable
 fun MyCarsScreen(
@@ -88,17 +100,31 @@ fun CardCar(car: CarDataModel, navController: NavController?) {
         colors = CardDefaults.cardColors(MaterialTheme.colorScheme.primaryContainer),
         onClick = { navController?.navigate("car_info") }
     ) {
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(8.dp)
-                .height(150.dp)
+
+                .height(200.dp)
         ) {
             Text(
                 text = car.name,
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
                 maxLines = 1,
-                textAlign = TextAlign.Center
+                textAlign = TextAlign.Center,
+                fontSize = 26.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Image(
+
+                painter = painterResource(id = R.drawable.ic_car),
+                contentDescription = "Car brand",
+                modifier = Modifier
+                    .wrapContentSize(Alignment.CenterStart)
+                    .fillMaxHeight()
+                    .size(100.dp)
+                    .padding(16.dp)
             )
             Column(
                 modifier = Modifier
@@ -106,9 +132,29 @@ fun CardCar(car: CarDataModel, navController: NavController?) {
                     .wrapContentSize(Alignment.CenterEnd)
                     .padding(end = 32.dp)
             ) {
-                Text(text = car.model)
-                Text(text = car.year)
+                Text(text = car.model, fontSize = 22.sp)
+                Text(text = car.year, fontSize = 22.sp)
             }
+            Text(
+                text = "Presiona para ver más",
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.onPrimaryContainer),
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onPrimary,
+            )
         }
     }
+}
+
+@Composable
+@Preview
+fun PreviewCard() {
+    CardCar(
+        CarDataModel(
+            "My Yaris", "Toyota", "Yaris", "2001",
+            "100", "100", "Melvin", false
+        ), null
+    )
 }
