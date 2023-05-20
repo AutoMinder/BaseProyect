30ca7bb8767a11608485b4bb3e3897bf69da2887
renegz <00077321@uca.edu.sv>
2023-05-19 18:09:46 -0600

    Car Model added

---
+++ AutoMinder/app/src/main/java/com/autominder/autominder/addcar/data/CarModel.kt
@@ -0,0 +1,10 @@
+package com.autominder.autominder.addcar.data
+
+data class CarModel(
+    //TODO(): Temporally model, possibly will change
+    val profileName: String,
+    val carYear: String,
+    val carKilometers: String,
+    val carLastOilChange: String,
+    val carLastMaintenance: String
+)
