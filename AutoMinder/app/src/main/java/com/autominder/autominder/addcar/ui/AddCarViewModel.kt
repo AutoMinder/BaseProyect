3781ae6282038d25dd86bd2ce4e08bf24a22846b
MelvinDiaz <00008821@uca.edu.sv>
2023-05-22 23:05:06 -0600

    Add: coroutines and stateflow to add car

--- AutoMinder/app/src/main/java/com/autominder/autominder/addcar/ui/AddCarViewModel.kt
+++ AutoMinder/app/src/main/java/com/autominder/autominder/addcar/ui/AddCarViewModel.kt
@@ -5,12 +5,15 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.autominder.autominder.AutoMinderApplication
 import com.autominder.autominder.addcar.data.AddCarRepository
 import com.autominder.autominder.addcar.data.CarModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class AddCarViewModel(
     private val repository: AddCarRepository,
@@ -18,45 +21,91 @@ class AddCarViewModel(
 ) : ViewModel() {
 
     private val _profileCarName = MutableStateFlow("")
-    val profileCarName: Flow<String> = _profileCarName
+    val profileCarName: StateFlow<String> = _profileCarName
 
     //TODO(): Se deben recibir las marcas del API
 
     private val _carBrand = MutableStateFlow("")
-    val carBrand: Flow<String> = _carBrand
+    val carBrand: StateFlow<String> = _carBrand
     //TODO(): Se deben recibir los modelos del API
 
     private val _carModel = MutableStateFlow("")
-    val carModel: Flow<String> = _carModel
+    val carModel: StateFlow<String> = _carModel
 
     private val _carYear = MutableStateFlow("")
-    val carYear: Flow<String> = _carYear
+    val carYear: StateFlow<String> = _carYear
 
     private val _carKilometers = MutableStateFlow("")
-    val carKilometers: Flow<String> = _carKilometers
+    val carKilometers: StateFlow<String> = _carKilometers
 
     private val _carLastOilChange = MutableStateFlow("")
-    val carLastOilChange: Flow<String> = _carLastOilChange
+    val carLastOilChange: StateFlow<String> = _carLastOilChange
 
     private val _carLastMaintenance = MutableStateFlow("")
-    val carLastMaintenance: Flow<String> = _carLastMaintenance
+    val carLastMaintenance: StateFlow<String> = _carLastMaintenance
 
-    private val _newCar = MutableStateFlow(CarModel(
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        ""
-    ))
+
+    private val _newCar = MutableStateFlow(
+        CarModel(
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        )
+    )
     val newCar: Flow<CarModel> = _newCar
 
     private val _addCarEnable = MutableStateFlow(false)
-    val addCarEnable: Flow<Boolean> = _addCarEnable
+    val addCarEnable: StateFlow<Boolean> = _addCarEnable
 
-    val carBrandsList = repository.getCarBrands()
-    val carModelsList = repository.getCarModels()
+    private val _carBrandsList = MutableStateFlow<List<String>>(emptyList())
+    val carBrandsList: StateFlow<List<String>> = _carBrandsList
+
+    private val _carModelsList = MutableStateFlow<List<String>>(emptyList())
+    val carModelsList: StateFlow<List<String>> = _carModelsList
+
+    private val _isLoading = MutableStateFlow<Boolean>(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    init {
+        fetchCarModels()
+        fetchBrands()
+    }
+
+    //TODO(): Clasificar por modelos
+    private fun fetchCarModels() {
+        viewModelScope.launch {
+            try {
+                setLoading(true)
+                _carModelsList.value = repository.getCarModels()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                setLoading(false)
+            }
+        }
+
+    }
+
+    fun fetchBrands() {
+        viewModelScope.launch {
+            try {
+                setLoading(true)
+                _carBrandsList.value = repository.getCarBrands()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                setLoading(false)
+            }
+        }
+    }
+
+    fun setLoading(isLoading: Boolean) {
+        _isLoading.value = isLoading
+    }
 
     fun onAddCarChange(
         profileCarName: String,
@@ -75,14 +124,31 @@ class AddCarViewModel(
         _carLastOilChange.value = carLastOilChange
         _carLastMaintenance.value = carLastMaintenance
         _newCar.value =
-            CarModel(profileCarName, carBrand, carModel, carYear, carKilometers, carLastOilChange, carLastMaintenance)
+            CarModel(
+                profileCarName,
+                carBrand,
+                carModel,
+                carYear,
+                carKilometers,
+                carLastOilChange,
+                carLastMaintenance
+            )
 
-        _addCarEnable.value = validFields(profileCarName, carBrand, carModel, carYear, carKilometers, carLastOilChange, carLastMaintenance)
+        _addCarEnable.value = validFields(
+            profileCarName,
+            carBrand,
+            carModel,
+            carYear,
+            carKilometers,
+            carLastOilChange,
+            carLastMaintenance
+        )
     }
 
     fun addCar(newCar: CarModel) {
 
         repository.addCar(newCar)
+        repository.getCars()
         Log.d("APP TAG", getCars().toString())
     }
 
@@ -97,7 +163,7 @@ class AddCarViewModel(
         carLastOilChange: String,
         carLastMaintenance: String
     ): Boolean =
-        profileCarName != "" && carBrand !="" && carModel !="" && carYear != "" && carKilometers != "" && carLastOilChange != "" && carLastMaintenance != ""
+        profileCarName != "" && carBrand != "" && carModel != "" && carYear != "" && carKilometers != "" && carLastOilChange != "" && carLastMaintenance != ""
 
     companion object {
         val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
--- AutoMinder/app/src/main/java/com/autominder/autominder/myCars/data/dummydatamycars.kt
+++ AutoMinder/app/src/main/java/com/autominder/autominder/myCars/data/dummydatamycars.kt
@@ -1,5 +1,5 @@
 package com.autominder.autominder.myCars.data
 
-val myCarsdummy = listOf<CarDataModel>(
+val myCarsdummy = mutableListOf<CarDataModel>(
     CarDataModel(1,"sdlfkj", "toyota", "yaris", "2001", "100", "01/01/2002", "Melvin", false),
 )
