9fc96ee1a7cad196bdb786ccc7f30989eb1230fd
MelvinDiaz <00008821@uca.edu.sv>
2023-05-26 19:32:04 -0600

    Add: calendar picker, to fix value change

--- AutoMinder/app/src/main/java/com/autominder/autominder/addcar/ui/AddCarScreen.kt
+++ AutoMinder/app/src/main/java/com/autominder/autominder/addcar/ui/AddCarScreen.kt
@@ -1,7 +1,11 @@
 package com.autominder.autominder.addcar.ui
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.widget.DatePicker
 import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,30 +13,59 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.SnackbarHost
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.TextButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.autominder.autominder.R
 import com.autominder.autominder.addcar.data.CarModel
 
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+
+
 @Composable
-fun AddCarScreen(viewModel: AddCarViewModel = viewModel(factory = AddCarViewModel.Factory), navController: NavController) {
+fun AddCarScreen(
+    viewModel: AddCarViewModel = viewModel(factory = AddCarViewModel.Factory),
+    navController: NavController
+) {
     Column(modifier = Modifier.fillMaxSize()) {
         HeaderText()
         AddCarForm(viewModel, navController)
@@ -313,24 +346,94 @@ fun CarDistance(carKilometers: String, onAddCarChange: (String) -> Unit) {
     }
 }
 
+
 @Composable
 fun CarLastOilChange(carLastOilChange: String, onAddCarChange: (String) -> Unit) {
     //TODO(): Hacer con dummydata y luego con la API
+    val openDialog = remember { mutableStateOf(false) }
+    val snackState = remember { SnackbarHostState() }
+    val snackScope = rememberCoroutineScope()
+    val datePicked = remember { mutableStateOf("") }
 
+    val year: Int
+    val month: Int
+    val day: Int
+
+    val calendar = Calendar.getInstance()
+    year = calendar.get(Calendar.YEAR)
+    month = calendar.get(Calendar.MONTH)
+    day = calendar.get(Calendar.DAY_OF_MONTH)
+
+    val date = remember { mutableStateOf("") }
+    val dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+
+    val datePickerState = rememberDatePickerState()
+    SnackbarHost(hostState = snackState, Modifier)
+    if (openDialog.value) {
+
+        DatePickerDialog(
+            onDismissRequest = {
+                openDialog.value = false
+
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        openDialog.value = false
+
+                    },
+                ) {
+                    Text("OK")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        openDialog.value = false
+                    }
+                ) {
+                    Text("Cancel")
+                }
+            }
+        ) {
+            DatePicker(
+                state = datePickerState,)
+            val pickedDate = datePickerState.selectedDateMillis
+
+        }
+
+    }
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
-        contentAlignment = Alignment.Center
-    ) {
+            .padding(16.dp)
+            .clickable { openDialog.value = true },
+        contentAlignment = Alignment.Center,
+
+        ) {
         OutlinedTextField(
-            value = carLastOilChange,
+            value = date.value,
             onValueChange = {
                 onAddCarChange(it)
             },
             label = { Text(text = "Ultimo cambio de aceite") },
-            placeholder = { Text(text = "dd-mm-aaaa") },
+            modifier = Modifier
+                .pointerInput(Unit) {}
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null
+                ) { openDialog.value = true },
+            readOnly = true,
+            enabled = false,
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = com.google.android.material.R.drawable.material_ic_calendar_black_24dp),
+                    contentDescription = ""
+                )
+            }
         )
+
+
     }
 }
 
@@ -351,7 +454,8 @@ fun CarLastMaintenance(carLastMaintenance: String, onAddCarChange: (String) -> U
             },
             label = { Text(text = "Ultimo mantenimiento realizado") },
             placeholder = { Text(text = "dd-mm-aaaa") },
-        )
+
+            )
     }
 }
 
