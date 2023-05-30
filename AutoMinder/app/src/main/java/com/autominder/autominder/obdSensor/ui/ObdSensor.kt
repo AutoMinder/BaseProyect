9cb0bff3e0623d2047f84abb78d8523db8ab4b04
MelvinDiaz <00008821@uca.edu.sv>
2023-05-29 18:56:04 -0600

    Add: bluetooth logic, not working yet :c

--- AutoMinder/app/src/main/java/com/autominder/autominder/obdSensor/ui/ObdSensor.kt
+++ AutoMinder/app/src/main/java/com/autominder/autominder/obdSensor/ui/ObdSensor.kt
@@ -1,12 +1,66 @@
 package com.autominder.autominder.obdSensor.ui
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavHostController
 
+
+@SuppressLint("MissingPermission")
 @Composable
 fun ObdSensorConnectScreen(
     obdSensorViewModel: ObdSensorViewModel,
     navController: NavHostController
 ) {
+    val context = LocalContext.current
+    val bluetoothEnabled by obdSensorViewModel.bluetoothEnabled.collectAsState(false)
+    val discoveredDevices by obdSensorViewModel.discoveredDevices.collectAsState(emptyList())
 
+
+    LaunchedEffect(Unit) {
+        val permissionGranted = ActivityCompat.checkSelfPermission(
+            context,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) == android.content.pm.PackageManager.PERMISSION_GRANTED
+
+        if (permissionGranted) {
+            obdSensorViewModel.verifyBluetoothEnabled(context)
+            obdSensorViewModel.startBluetoothDiscovery(context)
+        } else {
+            ActivityCompat.requestPermissions(
+                context as Activity,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                200
+            )
+        }
+    }
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp)
+
+    ) {
+        items(discoveredDevices.size) {
+            val device = discoveredDevices.getOrNull(it)
+            if (!bluetoothEnabled) {
+                Text(text = "Bluetooth is not enabled")
+            } else {
+
+                Text(text = device?.name ?: "Unknown Device")
+            }
+
+        }
+    }
 }
--- AutoMinder/app/src/main/java/com/autominder/autominder/obdSensor/ui/ObdSensorViewModel.kt
+++ AutoMinder/app/src/main/java/com/autominder/autominder/obdSensor/ui/ObdSensorViewModel.kt
@@ -3,27 +3,48 @@ package com.autominder.autominder.obdSensor.ui
 import android.annotation.SuppressLint
 import android.app.Application
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.Intent
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class ObdSensorViewModel() : ViewModel() {
     private val _bluetoothEnabled = MutableStateFlow(false)
     val bluetoothEnabled: StateFlow<Boolean> = _bluetoothEnabled
 
+    private val _discoveredDevices = MutableStateFlow(emptyList<BluetoothDevice>())
+    val discoveredDevices: StateFlow<List<BluetoothDevice>> = _discoveredDevices
 
     fun verifyBluetoothEnabled(context: Context) {
         val bluetoothManager =
             context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager?
         val bluetoothAdapter = bluetoothManager?.adapter
         _bluetoothEnabled.value = bluetoothAdapter?.isEnabled ?: false
+    }
+
+    fun startBluetoothDiscovery(context: Context) {
+        val bluetoothAdapter =
+            (context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter
+        if (ContextCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == android.content.pm.PackageManager.PERMISSION_GRANTED
+        ) {
+            viewModelScope.launch {
+                bluetoothAdapter.startDiscovery()
+                _discoveredDevices.value = bluetoothAdapter.bondedDevices.toList()
+            }
+        }
     }
 
 
